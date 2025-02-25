75f5682de330b9cb0fb87c11b9878f6ffc4a4a19
znkim <znkim@gaia3d.com>
2025-02-25 16:48:51 +0900

    feat : update entry.js

--- src/entry.js
+++ src/entry.js
@@ -1,5 +1,7 @@
 import { MagoViewer } from "./cesium/MagoViewer.js";
 import { MagoWaterSimulation } from "./cesium/water/MagoWaterSimulation.js";
 import { MagoWaterSimulationOptions } from "./cesium/water/MagoWaterSimulationOptions.js";
+import { MagoSSAO } from "./cesium/MagoSSAO.js";
+import { MagoEdge } from "./cesium/MagoEdge.js";
 
-export { MagoViewer, MagoWaterSimulation, MagoWaterSimulationOptions };
+export { MagoViewer, MagoWaterSimulation, MagoWaterSimulationOptions, MagoSSAO, MagoEdge };
